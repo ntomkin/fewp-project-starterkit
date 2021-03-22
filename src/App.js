967afe8eef9db871e9b6bbbf795dde96d3de4721
Nick Tomkin <nick@orchestrated.ca>
2021-03-22 01:19:23 -0400

    Application update

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: null};
+
+    this.getDataExample();
+  }
+
+  //  This example shows us how to get data from our database
+  //  Note: in order for this to work, you need to deploy this
+  //  application at least once, so that `app/data.php` exists
+  //  on Heroku
+  getDataExample() {
+    // https://www.npmjs.com/package/axios#example
+    axios.get(process.env.REACT_APP_URL + '/data.php')
+      .then(function(res) {
+        console.log(res);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+    }
+}
+
+export default App;
