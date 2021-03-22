6022a7256b5b8523268f719c4a5e8e0f10f102b6
Nick Tomkin <nick@orchestrated.ca>
2021-03-22 14:14:03 -0400

    Application update

--- public/data.php
+++ public/data.php
@@ -35,48 +35,65 @@ class DatabaseConnection {
 
   function __construct() {
     $this->connect();
-    $this->test();
+    if(!$this->test()) $this->setup();
   }
-
+  
+  //  Get a row from the records table
   public function get($id) {
     //  We both know this isn't good enough, as it isn't filtering. Please make sure you do that :)
-    $results = pg_query($this->getConnection(), "SELECT * FROM records WHERE id = $id");
+    $results = pg_query($this->getConnection(), "SELECT * FROM records WHERE id = $id ORDER BY name LIMIT 1");
     $row = pg_fetch_assoc($results, 0);
-
     return $row;
   }
 
-  public function create($name, $amazingLevel, $country) {
-    //  Creates a row in the records table
-    pg_prepare($this->getConnection(), "create_record", "INSERT INTO records (name, amazing_level, country) VALUES ($1, $2, $3);");
-    pg_execute($this->getConnection(), "create_record", array($name, $amazingLevel, $country));
+  //  Gets all rows from the records table
+  public function all() {
+    //  We both know this isn't good enough, as it isn't filtering. Please make sure you do that :)
+    $results = pg_query($this->getConnection(), "SELECT * FROM records ORDER BY name");
+    $rows = pg_fetch_assoc($results);
+    return $rows;
   }
 
+  //  Creates a row in the records table
+  public function create($name, $amazingLevel, $country) {
+    try {
+      pg_prepare($this->getConnection(), "create_record", "INSERT INTO records (name, amazing_level, country) VALUES ($1, $2, $3);");
+      pg_execute($this->getConnection(), "create_record", array($name, $amazingLevel, $country));
+      return TRUE;
+    } catch(Exception $e) {
+      return FALSE;
+    }
+  }
+
+  //  Establish connection to database
   function connect() {
     $this->connection = pg_connect($this->getConnectionString());
   }
 
+  //  Get reference to current connection
   function getConnection() {
     return $this->connection;
   }
 
-  function test() {
-    try {
-      //  Test if records table exists
-      $results = pg_query($this->getConnection(), "SELECT * FROM records LIMIT 1");
-      if(!$results) $this->setup();
-      $row = pg_fetch_row($results, 0);
-      return $row;
-    } catch(Exception $e) {
-      $this->setup();
-    }
+  //  Get database connection string
+  function getConnectionString() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
   }
 
+  //  Test if records table exists
+  function test() {
+    $results = pg_query($this->getConnection(), "SELECT * FROM records LIMIT 1");
+    return $results ? TRUE : FALSE;
+  }
+
+  //  Creates record table and inserts a few fake records
   function setup() {
+    
     //  Don't run if things are all setup
     if($this->test()) return;
 
-    //  Creates record table
+    //  Prepare SQL statement for creating a table called 'records'
     pg_prepare($this->getConnection(), "create_table", "CREATE TABLE IF NOT EXISTS records (
       id SERIAL PRIMARY KEY,
       name CHARACTER VARYING(100),
@@ -84,42 +101,14 @@ class DatabaseConnection {
       country CHARACTER VARYING(100)
     );");
 
+    //  Execute prepared statement
     pg_execute($this->getConnection(), "create_table", array());
 
-    //  Create some fake records to read
+    //  Create some fake records
     $this->create("Kylie Minogue", 10, "Australia");
     $this->create("Sugababes", 9, "England");
     $this->create("2 Unlimited", 8, "Germany");
     $this->create("Brooklyn Bounce", 8, "United States");
   }
 
-  function getConnectionString() {
-    extract(parse_url($_ENV["DATABASE_URL"]));
-    return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
-  }
-
 }
-
-
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-// function pg_connection_string_from_database_url() {
-//   extract(parse_url($_ENV["DATABASE_URL"]));
-//   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-// }
-
-# Here we establish the connection. Yes, that's all.
-// $pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-# Now let's use the connection for something silly just to prove it works:
-// $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-
-// print "<pre>\n";
-// if (!pg_num_rows($result)) {
-//       print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-// } else {
-//   print "Tables in your database:\n";
-//   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-// }
-// print "\n";
-?>
