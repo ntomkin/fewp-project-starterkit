bdea7b9d54339ef0ee853755d51a14c00b0c252c
Nick Tomkin <nick@orchestrated.ca>
2021-03-22 03:12:11 -0400

    Application update

--- public/data.php
+++ public/data.php
@@ -75,12 +75,14 @@ class DatabaseConnection {
 
   function setup() {
     //  Creates record table
-    pg_execute($this->getConnectionString(), ["CREATE TABLE IF NOT EXISTS records (
+    $result = pg_prepare($this->getConnection(), "create_table", "CREATE TABLE IF NOT EXISTS records (
       id SERIAL PRIMARY KEY,
       name CHARACTER VARYING(100),
       amazing_level INT,
       country CHARACTER VARYING(100)
-    );"]);
+    );");
+
+    $result = pg_execute($this->getConnection(), "create_table");
   }
 
   function getConnectionString() {
