0edc55f88a138f58687a4528537dc1a75df58267
Nick Tomkin <nick@orchestrated.ca>
2021-03-22 13:49:07 -0400

    Application update

--- build/data.php
+++ build/data.php
@@ -48,8 +48,7 @@ class DatabaseConnection {
 
   public function create($name, $amazingLevel, $country) {
     //  Creates record table
-    pg_prepare($this->getConnection(), "create_record", "INSERT INTO records (name, amazing_level, country) VALUES ('?', ?, '?');");
-
+    pg_prepare($this->getConnection(), "create_record", "INSERT INTO records (name, amazing_level, country) VALUES ($1, $2, $3);");
     pg_execute($this->getConnection(), "create_record", array($name, $amazingLevel, $country));
   }
 
